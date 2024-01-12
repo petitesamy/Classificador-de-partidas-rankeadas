1935be363226f8b776e98f7f1673a40db5cfc879
petitesamy <samanthalbs12@gmail.com>
2024-01-11 23:02:16 -0300

    primeiro commit

---
+++ classificador.js
@@ -0,0 +1,29 @@
+function calcularRank(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas
+    let nivel 
+
+    if (saldoVitorias < 10) {
+        nivel = "Ferro"
+    } else if (saldoVitorias >= 10 && saldoVitorias <= 20) {
+        nivel = "Bronze"
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+        nivel = "Prata"
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+        nivel = "Ouro"
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+        nivel = "Diamante"
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+        nivel = "Lendário"
+    } else {
+        nivel = "Imortal"
+    }
+
+    let mensagem = "O Herói tem um saldo de " + saldoVitorias + " e está no nível de " + nivel
+    return mensagem;
+}
+
+
+let vitorias = 123
+let derrotas = 63
+let resultado = calcularRank(vitorias, derrotas)
+console.log(resultado)
